eeb8522be6b14bae272f536777cf702e99817c8b
Mrylabs <mehryt71@gmail.com>
2025-10-17 22:11:52 +0330

    data: add more products to the dataset

--- JS/products.js
+++ JS/products.js
@@ -16,8 +16,49 @@ export const products = [
   },
   {
     title: "Smart Watch",
-    price: 249,
+    price: 249.99,
     img: "https://m.media-amazon.com/images/I/71MHu1Rtu0L._AC_SX569_.jpg"
+  },
+  {
+    title: "Tablet",
+    price: 198.15,
+    img: "https://m.media-amazon.com/images/I/51Z2kw-gFVL._AC_SY300_SX300_QL70_FMwebp_.jpg"
+  },
+  {
+    title: "Lenovo Tablet",
+    price: 289.25,
+    img: "https://m.media-amazon.com/images/I/714VRmqcVmL._AC_SX425_.jpg"
+  },
+  {
+    title: "PlayStation 5",
+    price: 495,
+    img: "https://m.media-amazon.com/images/I/61g2DrKiJsL._AC_SX425_.jpg"
+  },
+  {
+    title: "Galaxy S25 Ultra",
+    price: 698.99,
+    img: "https://m.media-amazon.com/images/I/61n0lmxP5-L._AC_SY300_SX300_QL70_FMwebp_.jpg"
+  },
+  {
+    title: "Amazon Kindle",
+    price: 198.99,
+    img: "https://m.media-amazon.com/images/I/51-PfgC9RBL._AC_SX679_.jpg"
+  },
+  {
+    title: "Apple Watch",
+    price: 333.99,
+    img: "https://m.media-amazon.com/images/I/61nNNMI1cFL._AC_SX342_SY445_QL70_FMwebp_.jpg"
+  },
+  {
+    title: "Virtual Reality Headset",
+    price: 445,
+    img: "https://m.media-amazon.com/images/I/61K1C1wqUSL._AC_SX425_.jpg"
+  },
+  {
+    title: "Android Tablet",
+    price: 100,
+    img: "https://m.media-amazon.com/images/I/71U9k+F44tL._AC_SX425_.jpg"
   }
+
 ];
 
